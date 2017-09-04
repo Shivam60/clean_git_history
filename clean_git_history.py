4407d05ef88da8e9b136f0461799ccdce398c516
shivam60 <shivamchawla60@gmail.com>
2017-09-05 01:31:27 +0530

    version 1

---
+++ clean_git_history.py
@@ -0,0 +1,12 @@
+import subprocess
+print("")
+p=subprocess.run('bash find_fattest_objects.sh n5 -d -f'.split(' '),stdout=subprocess.PIPE))
+out,err=p.communicate()
+for i in out:
+ print(i)
+i=input("Enter y to clean the above mentioned from git history. anything else to stop.")
+if i=='y':
+ for i in out:
+  p=subprocess.run('bash purge_objects.sh'.split(' ')+list(i))
+else:
+ print("Stop input read. bye")
